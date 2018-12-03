79e9befda4f075e18b61ff72ae452dca0989bf2a
svlandeg <sofie.vanlandeghem@gmail.com>
2018-12-03 12:59:17 +0100

    sampling 20 exercises at random and shuffling them at random

--- src/be/svlandeg/oxyfit/InputReader.kt
+++ src/be/svlandeg/oxyfit/InputReader.kt
@@ -1,11 +1,25 @@
 package be.svlandeg.oxyfit
 
 import tech.tablesaw.api.Table
+import tech.tablesaw.api.IntColumn
 import tech.tablesaw.io.csv.CsvReadOptions
+
+
 
 fun main(args : Array<String>) {
     val builder = CsvReadOptions.builder("data/input/exercises.tab").separator('\t')
-    val x = Table.read().csv(builder.build())
+    val exTable = Table.read().csv(builder.build()).dropRowsWithMissingValues()
 
-    println(x)
+    // for now, assuming a simple sampling selection of 20 exercises
+    val nrRows = 20
+    var exSample = exTable.sampleN(nrRows);
+
+    // shuffle the 20 selected rows randomly
+    val indexList = (0..nrRows).toList().shuffled().toIntArray()
+    val exIndex = IntColumn.create("Index", indexList)
+    exSample.addColumns(exIndex)
+    exSample = exSample.sortOn("Index")
+
+    println(exSample)
+
 }
