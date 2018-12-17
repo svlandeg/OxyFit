368b7ee44bfb7b8a0ffe473919a46002d12fe276
svlandeg <sofie.vanlandeghem@gmail.com>
2018-12-17 18:44:44 +0100

    print different input values per column

--- src/be/svlandeg/oxyfit/io/InputReader.kt
+++ src/be/svlandeg/oxyfit/io/InputReader.kt
@@ -2,18 +2,42 @@ package be.svlandeg.oxyfit.io
 
 import tech.tablesaw.api.Table
 import tech.tablesaw.io.csv.CsvReadOptions
+import java.util.*
 
 /**
- * Class that reads a CSV table and does minimal preprocessing
+ * Class that reads a CSV table and performs preprocessing and filtering.
  *
  * @author Sofie Van Landeghem (OxyKodit)
  */
 class InputReader(private val location: String, private val sep: Char)
 {
-    // Read a CSV table and drop the missing values
+    /* Read a CSV table and drop the missing values */
     fun read(): Table
     {
         val builder = CsvReadOptions.builder(location).separator(sep)
         return Table.read().csv(builder.build()).dropRowsWithMissingValues()
+    }
+
+    /* Filter the rows of the table according to the gear you have available. This assumes a format of "gear1, gear2" per cell in the input */
+    fun filterGear(inputTable: Table, availableGear: Set<String>): Table
+    {
+        val lowerGear = availableGear.map { it.toLowerCase() }
+
+        //println(inputTable.stringColumn("Gear").lowerCase().split(",").map { it.trim() })
+        //val outputTable = inputTable.where(inputTable.stringColumn("Gear").lowerCase().asList().isIn(lowerGear))
+        return inputTable
+    }
+
+    /* Obtain the different values for a column, assuming a format of value1, value2 per cell in the input */
+    fun getValues(inputTable: Table, columnName: String): TreeSet<String>
+    {
+        var values = sortedSetOf<String>()
+
+        for (row in inputTable)
+        {
+            values.addAll(row.getString(columnName).split(",").map { it.toLowerCase().trim() })
+        }
+        return values
+
     }
 }
--- src/be/svlandeg/oxyfit/run/RunHardcoded.kt
+++ src/be/svlandeg/oxyfit/run/RunHardcoded.kt
@@ -12,11 +12,25 @@ fun main(args : Array<String>) {
     val inputFile = "data/input/exercises.tab"
     val sep = '\t'
 
-    val exTable = InputReader(inputFile, sep).read()
+    // val availableGear = SetOf<String>("None").toLowerCase()
+    val availableGear = setOf("None", "Kettlebell", "Dumbbell", "2 dumbbells", "Any weight", "Fitness mat", "Speed rope")
+
+    val reader = InputReader(inputFile, sep)
+    val exTable = reader.read()
     println("read " + exTable.rowCount() + " rows")
 
-    // for now, assuming a simple sampling selection of 20 exercises
-    val nrRows = 20
+    //val filteredTable = reader.filterGear(exTable, availableGear)
+    //println("filtered down to " + filteredTable.rowCount() + " rows")
+
+    /* print the gears, muscles and types present in the input */
+    println()
+    println("gears: " + reader.getValues(exTable, "Gear"))
+    println("muscles: " + reader.getValues(exTable, "Muscles"))
+    println("types: " + reader.getValues(exTable, "Type"))
+    println()
+
+    /* for now, assuming a simple sampling selection of 20 exercises */
+    val nrRows = 15
     val exSample = RandomSelection(exTable, nrRows).process()
     println("randomly selected " + exSample.rowCount() + " rows")
 
